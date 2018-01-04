65b85819e3988c60add0737e57bcf9b2d541b40e
ting723 <ting723@gmail.com>
2018-01-04 22:10:09 +0800

    初始化

---
+++ src/main/java/com/zhanglw/java8lambdas/Java8LambdasApplication.java
@@ -0,0 +1,12 @@
+package com.zhanglw.java8lambdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LambdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8LambdasApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhanglw/java8lambdas/Java8LambdasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhanglw.java8lambdas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Java8LambdasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
